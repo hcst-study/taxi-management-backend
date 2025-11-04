5e778b750f87ff0633672b158e6be51f9b92e2f9
VSR <hcststudyofficial00@gmail.com>
2025-11-04 07:08:53 +0530

    Add protected route for drivers using JWT middleware

--- src/routes/driverRoutes.js
+++ src/routes/driverRoutes.js
@@ -1,8 +1,22 @@
 const express = require('express');
-const { registerDriver, loginDriver } = require('../controllers/driverController');
+const {
+  registerDriver,
+  loginDriver
+} = require('../controllers/driverController');
+const { protect } = require('../middleware/authMiddleware');
+
 const router = express.Router();
 
+// Public routes
 router.post('/register', registerDriver);
 router.post('/login', loginDriver);
+
+// Protected route (JWT required)
+router.get('/profile', protect, (req, res) => {
+  res.json({
+    message: 'Protected route accessed successfully (driver)',
+    driver: req.user
+  });
+});
 
 module.exports = router;
